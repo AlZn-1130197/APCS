56aa8a90abb92c22319c814f68fb0a49d1272455
AlZn-1130197 <s1130197@dmhs.kh.edu.tw>
2026-04-07 12:00:34 +0800

    Create 2.排序 .cpp

---
+++ 2.排序 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[1000];
+	for(int i=0 ; i<n; i++) {
+		cin >> ar[i];
+	}
+	sort(arr, arr+n);
+	for (int i=0; i<n; i++) {
+		cout << [i] << " ";
+	}
+	cout << "\n";
+  }
+  return 0;
+}
